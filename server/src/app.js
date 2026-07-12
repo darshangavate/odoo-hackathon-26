578c8bf5ff6834964d38d36fcb0a4b584dc285f9
darshan <darshangavate9529@gmail.com>
2026-07-12 10:50:55 +0530

    Commit current workspace changes

---
+++ server/src/app.js
@@ -0,0 +1,29 @@
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+
+import authRoutes from "./routes/authRoutes.js";
+
+import notFound from "./middleware/notFound.js";
+import errorHandler from "./middleware/errorHandler.js";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+    res.status(200).json({
+        success: true,
+        message: "AssetFlow API Running",
+    });
+});
+
+app.use("/api/auth", authRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
+
+export default app;
